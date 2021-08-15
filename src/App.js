0ae329233c8c80a3531c20876768641b3ca5013c
mematthew123 <mematthew123@gmail.com>
2021-08-14 18:15:04 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./componenets/Navbar";
+import {BrowserRouter as Router} from 'react-router-dom';
+
+function App() {
+  return (
+    
+    <Router>
+<Navbar/>
+
+    </Router>
+  );
+}
+
+export default App;
